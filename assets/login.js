896c4ba7d5f39258359343f4c75a083474d9a885
Anton <madspy0@gmail.com>
2021-12-10 11:47:14 +0200

    add show hiden pass

--- assets/login.js
+++ assets/login.js
@@ -1,5 +1,8 @@
 import Swal from "sweetalert2";
 import Inputmask from "inputmask";
+//const bootstrap = require('bootstrap');
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import {swal_person} from "./draw/swal_person";
 //import '@algolia/autocomplete-theme-classic';
 //import {autocomplete} from '@algolia/autocomplete-js';
 
@@ -13,6 +16,21 @@ if(!!profilePhone) {
     Inputmask({mask: "http[s]://*{1,50}.*{1,30}[.*{1,30}]{1,10}", placeholder: "_"}).mask(document.getElementById("registration_form_profile_url"));
 }
 
+document.getElementById('toggle-pass').addEventListener('click', function (e) {
+    e.preventDefault()
+    //  my_modal(true)
+    let input_pass = document.getElementById('registration_form_plainPassword');
+    let toogler = document.getElementById('toggle-pass-icon')
+    if(toogler.classList.contains('bi-eye-slash')) {
+        toogler.classList.remove('bi-eye-slash')
+        toogler.classList.add('bi-eye')
+        input_pass.type = "text"
+    } else {
+        toogler.classList.remove('bi-eye')
+        toogler.classList.add('bi-eye-slash')
+        input_pass.type = "password"
+    }
+})
 // let otgAutocomplete = autocomplete({
 //     container: '#otg-registration',
 //     placeholder: 'Назва органу влади',
