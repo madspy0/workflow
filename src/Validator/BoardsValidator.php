6aea727ae3a957db55ff35e4631b01650336ad00
Anton <madspy0@gmail.com>
2021-12-13 12:05:42 +0200

    add oblast

--- src/Validator/BoardsValidator.php
+++ src/Validator/BoardsValidator.php
@@ -32,7 +32,8 @@ class BoardsValidator extends ConstraintValidator
             throw new UnexpectedValueException($value, 'string');
         }
         $repository = $this->em->getRepository(DrawnArea::class);
-        if(!$repository->inBoard($value)) {
+        dump($user->getProfile()->getOblast()->getKoatuu2());
+        if($repository->inBoard($value) != $user->getProfile()->getOblast()->getKoatuu2()) {
             //    dump($this->context->getPropertyPath('geom'));
             $this->context->buildViolation($constraint->message)
                 //               ->atPath('geom')
