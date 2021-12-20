92a42439a62f305b94c099e5da7e0accf77f048c
anton <cguhq098>
2021-12-20 02:50:57 +0200

    from we2

--- src/EventListener/LoginSubscriber.php
+++ src/EventListener/LoginSubscriber.php
@@ -3,24 +3,34 @@
 namespace App\EventListener;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use App\Entity\User;
 use Symfony\Component\Security\Http\SecurityEvents;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
     private $em;
+    private $flash;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, FlashBagInterface $flash)
     {
         $this->em = $em;
+        $this->flash=$flash;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
     {
+        if(!$event->getRequest()->get('g-recaptcha-response')) {
+            throw new Exception('recaptcha_error');
+        }
+
+        $this->flash->add('successlogged_info', 'logged');
         // Get the User entity.
         $user = $event->getAuthenticationToken()->getUser();
+
 
         // Update your field here.
         $user->setCurrentAt(new \DateTimeImmutable());
